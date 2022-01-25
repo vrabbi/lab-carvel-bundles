5d4130f9f96e7be3760ef9d6f6b7399c920f2a89
Scott Rosenberg <48493016+vrabbi@users.noreply.github.com>
2022-01-25 09:57:51 +0200

    Update update-carvel.sh

--- workshop/setup.d/update-carvel.sh
+++ workshop/setup.d/update-carvel.sh
@@ -28,7 +28,7 @@ PATH=/tmp/carvel:$PATH
 
 curl -L --silent --show-error --fail -o /tmp/carvel.sh https://carvel.dev/install.sh && \
     export K14SIO_INSTALL_BIN_DIR=/opt/kubernetes/bin && \
-    sh -x /tmp/carvel.sh && \
+    bash -x /tmp/carvel.sh && \
     rm /tmp/carvel.sh
 
 rm -rf /tmp/carvel

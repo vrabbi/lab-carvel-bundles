4bd6623cb5b8b7fbe205dbc58e5dca3475b7883e
Scott Rosenberg <48493016+vrabbi@users.noreply.github.com>
2022-01-25 15:52:12 +0200

    Create connect-vcluster.sh

---
+++ workshop/setup.d/connect-vcluster.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -x
+
+set -eo pipefail
+
+# Download vcluster binary.
+
+curl -L -o /opt/kubernetes/bin/vcluster https://github.com/loft-sh/vcluster/releases/download/v0.3.0-beta.2/vcluster-linux-amd64
+
+chmod +x /opt/kubernetes/bin/vcluster
+
+# Ensure using Educates cluster context.
+
+kubectl config use-context eduk8s
+
+# Connect to cluster, merging the kubeconfig with $HOME/.kube/config.
+
+vcluster connect vcluster -n $SESSION_NAMESPACE-vc --server https://vcluster.$SESSION_NAMESPACE-vc.svc.cluster.local --update-current
+
+# Set the current context to use the virtual Kubernetes cluster.
+
+kubectl config use-context vcluster_$SESSION_NAMESPACE-vc_vcluster 
+
+kubectl config set-context vcluster_$SESSION_NAMESPACE-vc_vcluster --namespace default

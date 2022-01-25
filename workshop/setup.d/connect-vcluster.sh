23032cc083e513d1f23056a418783cc1116682c6
Scott Rosenberg <48493016+vrabbi@users.noreply.github.com>
2022-01-25 16:24:44 +0200

    Update connect-vcluster.sh

--- workshop/setup.d/connect-vcluster.sh
+++ workshop/setup.d/connect-vcluster.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-set -x
-
-set -eo pipefail
-
 # Download vcluster binary.
 #curl -L --silent --show-error --fail -o /opt/kubernetes/bin/vcluster https://github.com/loft-sh/vcluster/releases/download/v0.3.0-beta.2/vcluster-linux-amd64 --retry 10 --retry-connrefused --retry-max-time 120 --retry-delay 5
 
